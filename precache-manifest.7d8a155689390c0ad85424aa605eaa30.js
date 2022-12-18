096002ee6d3c647008e981bb62cf3610a915f9b3
IshitwaAgrawal <ishitwa2000@gmail.com>
2022-12-18 21:14:50 +0000

    Updates

---
+++ precache-manifest.7d8a155689390c0ad85424aa605eaa30.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "9b67607be603adb19d3f",
+    "url": "/react-gh-actions-demo/static/css/main.9b5ef482.chunk.css"
+  },
+  {
+    "revision": "9b67607be603adb19d3f",
+    "url": "/react-gh-actions-demo/static/js/main.9b67607b.chunk.js"
+  },
+  {
+    "revision": "95ea83b6c7492a61ef7b",
+    "url": "/react-gh-actions-demo/static/js/1.95ea83b6.chunk.js"
+  },
+  {
+    "revision": "fe9e49dcc9c458a203f8",
+    "url": "/react-gh-actions-demo/static/js/2.fe9e49dc.chunk.js"
+  },
+  {
+    "revision": "8f9ca7afc32c1ae3c8f2",
+    "url": "/react-gh-actions-demo/static/js/runtime~main.8f9ca7af.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/react-gh-actions-demo/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "e723d769e23fd89758c6b481ffb2a272",
+    "url": "/react-gh-actions-demo/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/demoapp/precache-manifest.bdf00548de65464c0ef49fd4199330fb.js"
+  "/react-gh-actions-demo/precache-manifest.7d8a155689390c0ad85424aa605eaa30.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/demoapp/index.html", {
+workbox.routing.registerNavigationRoute("/react-gh-actions-demo/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
